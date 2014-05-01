69a65d6c351769bf897d43d3034839766889afdd
lpradel <lukas.pradel@googlemail.com>
2014-05-01 03:46:19 +0200

    Dual languages for legal disclosure.

--- app/lang/en/legal.php
+++ app/lang/en/legal.php
@@ -22,41 +22,43 @@ return array(
     // Disclaimer
     'disclaimer-heading'        => 'Disclaimer',
     'disclaimer-content'        => 'Liability for content',
-    'disclaimer-content-val'    => 'Als Diensteanbieter sind wir gem&auml;ß &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
-                                    Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen
-                                    zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.
-                                    Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
-                                    Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
-                                    Rechtsverletzung m&ouml;glich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
+    'disclaimer-content-val'    => 'As a service provider we are responsible for our own content on these websites in line with &sect; 7 section 1 TMG.
+                                    According to &sect;&sect; 8 to 10 TMG we are however as a service provider not required to monitor transmitted oder saved external information
+                                    or to look for circumstances that indicate illegal action.
+                                    Liabilities for removal or blocking of information in line with general laws remain unaffected by this.
+                                    However, a liability hereof is only possible from the date of awareness of a concrete infringement of law.
+                                    Upon learning of such infringements we will remove the content in question immediately.',
     'disclaimer-links'          => 'Liability for links',
-    'disclaimer-links-val'      => 'Unser Angebot enth&auml;lt Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben.
-                                    Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen.
-                                    F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.
-                                    Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;ße &uuml;berpr&uuml;ft.
-                                    Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
-                                    Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
-                                    Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
+    'disclaimer-links-val'      => 'Our websites contain links to websites of third parties the contents of which we cannot control.
+                                    Therefore we cannot accept liability for these external contents.
+                                    The respective provider or owner is liable for the contents of the linked websites.
+                                    The linked websites were examined for possible violations of laws at the time of linking.
+                                    Illegal content was not in evidence at that point in time.
+                                    A permanent contentual supervision of the contents of the linked sites is however unreasonable without clear leads.
+                                    Upon learning of violations of laws we will remove the links in question immediately.',
     'disclaimer-copyr'          => 'Copyright law',
-    'disclaimer-copyr-val'      => 'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht.
-                                    Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des
-                                    Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind
-                                    nur f&uuml;r den privaten, nicht kommerziellen Gebrauch gestattet.
-                                    Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.
-                                    Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um
-                                    einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.',
+    'disclaimer-copyr-val'      => 'The contents and works created by the persons running the website are subject to German copyright law.
+                                    Duplication, modification, distribution and every kind of exploitation beyond the boundaries of copyright law
+                                    require written consent of the author or creator respectively in question.
+                                    Downloads and copies of this site are only permitted for personal, non-commercial use.
+                                    As far as the contents of this site were not created by the person running the site the copyrights of third parties were heeded.
+                                    In particular contents of third parties are labelled as such.
+                                    If you should still become aware of copyright infringment we request you to send us a corresponding notice.
+                                    Upon learning of violations of laws we will remove the content in question immediately.',
     'disclaimer-source'         => 'Source',
 
     // Privacy policy
     'privacy-heading'           => 'Data Privacy Statement',
     'privacy-policy'            => 'Privacy policy',
-    'privacy-policy-val'        => 'Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten m&ouml;glich.
-                                    Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies,
-                                    soweit m&ouml;glich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdr&uuml;ckliche Zustimmung nicht an Dritte weitergegeben.
-                                    Wir weisen darauf hin, dass die Daten&uuml;bertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitsl&uuml;cken aufweisen kann.
-                                    Ein l&uuml;ckenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht m&ouml;glich.
-                                    Der Nutzung von im Rahmen der Impressumspflicht ver&ouml;ffentlichten Kontaktdaten durch Dritte zur &Uuml;bersendung von nicht ausdr&uuml;cklich
-                                    angeforderter Werbung und Informationsmaterialien wird hiermit ausdr&uuml;cklich widersprochen.
-                                    Die Betreiber der Seiten behalten sich ausdr&uuml;cklich rechtliche Schritte im Falle der unverlangten Zusendung von
-                                    Werbeinformationen, etwa durch Spam-Mails, vor.',
+    'privacy-policy-val'        => 'Using our website is typically possible without stating personal data.
+                                    Insofar as personal data (for example your name, address oder E-Mail address) is gathered on our site, this data is given
+                                    voluntarily if possible.
+                                    This data will not be given to third parties withour your explicit consent. We would like to make you aware that the transmission
+                                    of data over the internet (for example communication by E-Mail) may have security vulnerabilities. Protecting the data without any
+                                    security gaps whatsoever allowing third parties to access the data is impossible.
+                                    We hereby explicitly object to using the data we are legally required to provide as part of the disclaimer to send us advertisements
+                                    or information material we did not explicitly request from third parties.
+                                    The persons running the sites explicitly remain the rights to take legal action in case we are sent advertisement information
+                                    that was not explicitly requested (like for example spam mails).',
     'privacy-source'            => 'Source'
 );
